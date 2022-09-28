30feb884efffbd1c3733f68b53f507562896035a
Antonio Alan <tonio.alan07@gmail.com>
2022-09-28 10:02:06 -0300

    improved header and more styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-
+import "normalize.css"
 import Menu from './pages/Menu'
 
 function App() {
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,11 +1,24 @@
 import styles from "./Menu.module.scss";
 import logo from "../../assets/logo.svg";
+import Search from "./Search";
+import { useState } from "react";
+
 export default function Menu(){
+    const [search, setSearch] = useState("");
     return(
         <main>
-            <nav className={styles.menu}>
+            <nav className={styles.menuNav}>
                 <img src={logo} alt="aluroni logo"/>
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    The house of pasta
+                </div>
+            </header>
+            <section className={styles.menu}>
+                <h3 className={styles.menu__title}>Menu</h3>
+                <Search search={search} setSearch={setSearch} />
+            </section>
         </main>
     )
 }
