c6d063b946f82cdd46f156f2970de611ced08e07
Antonio Alan <tonio.alan07@gmail.com>
2022-12-16 10:20:50 -0300

    inicio de rotas de pratos

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import PaginaPadrao from 'components/PaginaPadao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import Sobre from 'pages/Sobre';
+import Prato from 'pages/Prato';
 import NotFound from 'pages/NotFound';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -19,7 +20,8 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre/>} />
           </Route>
-          <Route path='*' element={<NotFound />} />          
+          <Route path='*' element={<NotFound />} />
+          <Route path= 'prato/:id' element={<Prato />} />          
         </Routes>
         <Footer/>
       </Router>
